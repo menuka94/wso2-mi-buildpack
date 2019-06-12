13384295a0df38b409963a8685605d4a5ba02a9e
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2019-06-12 19:13:53 +0530

    update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,17 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-GOOS=linux go build -ldflags="-s -w" -o bin/supply wso2-mi/supply/cli
-GOOS=linux go build -ldflags="-s -w" -o bin/finalize wso2-mi/finalize/cli
+WSO2_MI_VERSION="1.0.0"
+
+BUILD_SCRIPT_DIR=$(dirname $(dirname $0))
+
+JDK_NAME="jdk-8u161-linux-x64.tar.gz"
+JDK_LOCATION=${BUILD_SCRIPT_DIR}/../resources/${JDK_NAME}
+
+echo ${BUILD_SCRIPT_DIR}
+if [[ ! -f ${JDK_LOCATION} ]]; then
+    echo "${JDK_LOCATION} is not available"
+    exit 1
+fi
+
+
